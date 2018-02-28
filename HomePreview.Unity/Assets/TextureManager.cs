58669609825fe8b0973dffcaeb1ae61254377982
chomado (Madoka Chiyoda) <madoka.madoka@microsoft.com>
2018-02-28 15:50:37 +0900

    Render 360 degree VR image when starting

---
+++ HomePreview.Unity/Assets/TextureManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        RenderSettings.skybox.mainTexture = (Texture)Resources.Load(path: "teidetour23_under_over_stereo_pair_4000");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
