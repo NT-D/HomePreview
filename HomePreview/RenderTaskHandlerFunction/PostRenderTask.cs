37d46f82fd9515f4d91378e7a45767c830c26e6d
Masayuki Ota <masota@microsoft.com>
2018-04-24 16:08:18 -0700

    add null handler for post data

--- HomePreview/RenderTaskHandlerFunction/PostRenderTask.cs
+++ HomePreview/RenderTaskHandlerFunction/PostRenderTask.cs
@@ -14,22 +14,33 @@ namespace RenderTaskHandlerFunction
     {
         [FunctionName(nameof(PostRenderTask))]
         public static IActionResult Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]HttpRequest req,
             [Queue(queueName: "render-task-queue", Connection = "AzureWebJobsStorage")] ICollector<string> QueueMessage,
             TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["roomsize"];
-
             string requestBody = new StreamReader(req.Body).ReadToEnd();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.roomsize;
-            QueueMessage.Add(JsonConvert.SerializeObject(data));
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            //1. Check wheter we got parameter. If it is null, return 400 bad request
+            if (data == null)
+            {
+                return new BadRequestObjectResult("please input room parameter");
+            }
+            else
+            {
+
+                //2. Check wheter same parameter in CosmosDB.
+
+                //2-1. If exists we return url to the client.
+
+                //2-2. If it doesn't exists, functions create new document in Cosmos DB and get uid, then send parameter and uid to Queue.
+                QueueMessage.Add(JsonConvert.SerializeObject(data));
+
+                //TODO: update 3. Return tetative url to the client
+                return (ActionResult)new OkObjectResult(data.roomsize);
+            }
         }
     }
 }
