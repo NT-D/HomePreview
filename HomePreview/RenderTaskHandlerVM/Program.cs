e5f14fe5513f00c25965312d0791db64ffbc1596
Ken N <kenagao@outlook.jp>
2018-04-25 16:58:21 -0700

    Check Storage queue and save the message into file (save does not work?)

--- HomePreview/RenderTaskHandlerVM/Program.cs
+++ HomePreview/RenderTaskHandlerVM/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
 
 namespace RenderTaskHandlerVM
 {
     class Program
     {
+        private static readonly string StorageUrl = "StorageUrl";
+        private static readonly string StorageKey = "StorageKey";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // create storage account
+            CloudStorageAccount storageAccount = new CloudStorageAccount(
+                new Microsoft.WindowsAzure.Storage.Auth.StorageCredentials(StorageUrl,StorageKey), true);
+
+            // start checking queue
+            new QueueCheck(storageAccount).RunAsync().Wait();
         }
     }
 }
---
+++ HomePreview/RenderTaskHandlerVM/QueueCheck.cs
@@ -0,0 +1,60 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+
+namespace RenderTaskHandlerVM
+{
+    class QueueCheck
+    {
+        private static readonly string _queueName = "render-task-queue";
+        private readonly CloudStorageAccount _account;
+
+        public QueueCheck(CloudStorageAccount account)
+        {
+            _account = account;          
+        }
+
+        public async Task<bool> RunAsync()
+        {
+            var client = _account.CreateCloudQueueClient();
+            var queue = client.GetQueueReference(_queueName);
+
+            // If there is no queue named _queueName, create the queue
+            await queue.CreateIfNotExistsAsync();
+
+            while (true)
+            {
+                await queue.FetchAttributesAsync();
+                int count = queue.ApproximateMessageCount ?? 0;
+
+                if (count > 0)
+                {
+                    // GetMessagesAsync support maximum count of 32
+                    if (count > 32)
+                        count = 32;
+
+                    foreach (var message in await queue.GetMessagesAsync(count))
+                    {
+                        if (message != null)
+                        {
+                            using (var file = new FileStream("param.json", FileMode.Create))
+                            using (var writer = new StreamWriter(file, Encoding.UTF8))
+                            {
+                                writer.Write(message.AsString);
+                            }
+                            await queue.DeleteMessageAsync(message);
+                        }
+                    }
+                    await Task.Delay(1000).ConfigureAwait(false);
+                }
+            }
+        }
+
+    }
+}
