7a6e32d29ad51cc19e30001677fb1095635660e7
chomado (Madoka Chiyoda) <madoka.madoka@microsoft.com>
2018-04-25 11:45:32 -0700

    SignalR : Server side

---
+++ HomePreview.WebApp/WebApp/Hubs/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Owin;
+
+namespace WebApp.Hubs
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
