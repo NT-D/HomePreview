bd3eb93e1b57d9cf462c10f7f2a436d17862eabe
chomado (Madoka Chiyoda) <madoka.madoka@microsoft.com>
2018-04-25 12:14:55 -0700

    moved my WebApp project to under the HomePreview solution

---
+++ HomePreview/HomePreview.Web/Hubs/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Owin;
+
+namespace HomePreview.Web.Hubs
+{ 
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
