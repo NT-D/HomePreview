0544566b21c8b62654b23463bf5fe3802717da32
chomado (Madoka Chiyoda) <madoka.madoka@microsoft.com>
2018-04-26 16:52:48 -0700

    to recieve user's inputs

--- HomePreview/HomePreview.Web/Controllers/HomeController.cs
+++ HomePreview/HomePreview.Web/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -18,6 +19,32 @@ namespace HomePreview.Web.Controllers
                 ImageUrl = "https://video.360cities.net/littleplanet-360-imagery/360Level43Lounge-8K-stable-noaudio-1024x512.jpg",
             };
             return View(viewModel);
+        }
+        [HttpPost]
+        public ActionResult OnSubmitButtonClicked(int Roomsize, string Windowsize)
+        {
+            Debug.WriteLine("l");
+            var viewModel = new Models.HomeViewModel()
+            {
+                Id = "test",
+                Roomsize = Roomsize,
+                Windowsize = ConvertWindowSizeToInt(Windowsize),
+                ImageUrl = "https://video.360cities.net/littleplanet-360-imagery/360Level43Lounge-8K-stable-noaudio-1024x512.jpg",
+            };
+            return View("Index", viewModel);
+        }
+        private int ConvertWindowSizeToInt(string windowSize)
+        {
+            switch (windowSize)
+            {
+                case "big":
+                    return 3;
+                case "medium":
+                    return 2;
+                case "small":
+                    return 1;
+            }
+            return 0;
         }
     }
 }
