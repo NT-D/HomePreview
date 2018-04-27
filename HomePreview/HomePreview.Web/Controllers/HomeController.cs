358f531435a15b86b91c810cd68b871456f5ff74
chomado (Madoka Chiyoda) <madoka.madoka@microsoft.com>
2018-04-26 18:41:48 -0700

    call Function API

--- HomePreview/HomePreview.Web/Controllers/HomeController.cs
+++ HomePreview/HomePreview.Web/Controllers/HomeController.cs
@@ -1,7 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -21,7 +26,7 @@ namespace HomePreview.Web.Controllers
             return View(viewModel);
         }
         [HttpPost]
-        public ActionResult OnSubmitButtonClicked(int Roomsize, string Windowsize)
+        public async Task<ActionResult> OnSubmitButtonClicked(int Roomsize, string Windowsize)
         {
             Debug.WriteLine("l");
             var viewModel = new Models.HomeViewModel()
@@ -31,8 +36,29 @@ namespace HomePreview.Web.Controllers
                 Windowsize = ConvertWindowSizeToInt(Windowsize),
                 ImageUrl = "https://video.360cities.net/littleplanet-360-imagery/360Level43Lounge-8K-stable-noaudio-1024x512.jpg",
             };
+            var imageUrl = await RequestRenderAsync();
+            viewModel.ImageUrl = imageUrl;
             return View("Index", viewModel);
         }
+        private static async Task<string> RequestRenderAsync()
+        {
+            var renderRequestApiUrl = ConfigurationManager.AppSettings["FunctionsEndPoint"];
+            string imageUrl = "";
+
+            using (var client = new HttpClient())
+            {
+                var json = @"
+{
+    'roomsize':22,
+    'windowsize':11
+}";
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                var response = await client.PostAsync(renderRequestApiUrl, content);
+                imageUrl = await response.Content.ReadAsStringAsync();
+            }
+            return imageUrl;
+        }
+
         private int ConvertWindowSizeToInt(string windowSize)
         {
             switch (windowSize)
